c717bd9c1c1955fc6f9418a278fd4b8888f27a5f
Emma Kester <catherine.emmak@gmail.com>
2023-10-08 18:37:25 -0700

    More front-end-back end communication

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import Table from "./Table";
 import Form from './form';
 
+
 function MyApp() {
   const [characters, setCharacters] = useState([]);
     useEffect(() => {
@@ -25,6 +26,26 @@ function MyApp() {
     function fetchUsers() {
         const promise = fetch("http://localhost:8000/users");
         return promise;
+    }
+
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
+
+        return promise;
+    }
+
+    function updateList(person) {
+        postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
 
